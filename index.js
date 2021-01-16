c48e2c85e599f6c1004190a58e920ed4ab88f594
ShahjamalMalik <shahjamalmalik@gmail.com>
2021-01-16 19:42:14 +0000

    Moved initial index.js code to iss.js file and exported the fetchMyIP function and call it in the index.js file

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const request = require('request');
+const { fetchMyIP } = require('./iss');
 
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org?format=json', (err, response, body) => {
-    if(err) {
-      callback(`This is the error message: ${err}`, null);
-    } else if(body) {
-      callback(null, body["ip"]);
-    } else {
-      console.log("Not working")
-    }
-  })
-}
-
-
-
+fetchMyIP((error, ip) => {
+  if(error) {
+    console.log("Error message: ", error);
+    return;
+  } else {
+    console.log("This is the ip address: ", ip);
+    return;
+  }
+})
